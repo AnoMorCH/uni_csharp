04e11c929a29dfc426ef012a4f8afc936b7b7159
Anton Morozov <anomorch@gmail.com>
2023-02-16 19:51:13 +0300

    Add ShowConsoleMenu() method

--- lab1/Program.cs
+++ lab1/Program.cs
@@ -6,7 +6,16 @@ namespace FirstLab // Note: actual namespace depends on the project name.
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            ShowConsoleMenu();
+        }
+
+        static void ShowConsoleMenu()
+        {
+            Console.WriteLine("Информация по типам:");
+            Console.WriteLine("1 - Общая информация по типам");
+            Console.WriteLine("2 - Выбрать тип из списка");
+            Console.WriteLine("3 - Параметры консоли");
+            Console.WriteLine("0 - Выход из программы");
         }
     }
 }
