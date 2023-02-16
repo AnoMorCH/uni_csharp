e9ac349f99a601163845e86297f1e3f4ac9b1088
Anton Morozov <anomorch@gmail.com>
2023-02-16 21:10:25 +0300

    Write a dump version of clause 1

--- lab1/Program.cs
+++ lab1/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿// TODO. Clean code.
+
+using System;
 
 namespace FirstLab // Note: actual namespace depends on the project name.
 {
@@ -9,13 +11,8 @@ namespace FirstLab // Note: actual namespace depends on the project name.
             StartApp();
         }
 
-        static void StartApp() 
-        {
-            ShowConsoleMenu();
-            WaitInput();
-        }
 
-        static void WaitInput() {
+        static void ConsoleMenuWaitInput() {
             while(true)
             {
                 char inputedKey = char.ToLower(Console.ReadKey().KeyChar); 
@@ -28,7 +25,7 @@ namespace FirstLab // Note: actual namespace depends on the project name.
                         // SelectType();
                         break;
                     case '3':
-                        // ChangeConsoleView();
+                        ChangeConsoleView();
                         break;
                     default:
                         break;
@@ -36,13 +33,128 @@ namespace FirstLab // Note: actual namespace depends on the project name.
             }
         }
 
-        static void ShowConsoleMenu()
+        static void ChangeConsoleViewWaitInput() {
+            while(true) 
+            {
+                char inputedKey = char.ToLower(Console.ReadKey().KeyChar); 
+                switch(inputedKey)
+                {
+                    case '1':
+                        StartApp();
+                        break;
+                    case '2':
+                        ChangeTextColor();
+                        break;
+                    case '3':
+                        ChangeBackgroundColor();
+                        break;
+                }
+            }
+        }
+
+        static void ChangeTextColorByInput() {
+            while (true)
+            {
+                char inputedKey = char.ToLower(Console.ReadKey().KeyChar); 
+                switch(inputedKey) 
+                {
+                    case '1':
+                        Console.ForegroundColor = ConsoleColor.White;
+                        StartApp();
+                        break;
+                    case '2':
+                        Console.ForegroundColor = ConsoleColor.Blue;
+                        StartApp();
+                        break;
+                    case '3':
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        StartApp();
+                        break;
+                    case '4':
+                        Console.ForegroundColor = ConsoleColor.Black;
+                        StartApp();
+                        break;
+                }
+            }
+        }
+
+        static void ChangeBackgroundColorByInput() {
+            while (true)
+            {
+                char inputedKey = char.ToLower(Console.ReadKey().KeyChar); 
+                switch(inputedKey) 
+                {
+                    case '1':
+                        Console.BackgroundColor = ConsoleColor.White;
+                        StartApp();
+                        break;
+                    case '2':
+                        Console.BackgroundColor = ConsoleColor.Blue;
+                        StartApp();
+                        break;
+                    case '3':
+                        Console.BackgroundColor = ConsoleColor.Red;
+                        StartApp();
+                        break;
+                    case '4':
+                        Console.BackgroundColor = ConsoleColor.Black;
+                        StartApp();
+                        break;
+                }
+            }
+        }
+
+        static void StartApp() 
         {
-            Console.WriteLine("Информация по типам:");
+            ShowConsoleMenu();
+            ConsoleMenuWaitInput();
+        }
+
+        static void ChangeBackgroundColor()
+        {
+            ShowStandardColors();
+            ChangeBackgroundColorByInput();
+        }
+
+        static void ChangeConsoleView() 
+        {
+            ShowChangeConsoleViewMenu();
+            ChangeConsoleViewWaitInput();
+        }
+
+        static void ChangeTextColor()
+        {
+            ShowStandardColors();
+            ChangeTextColorByInput();
+        }
+
+        static void ShowStandardColors()
+        {
+            Console.Clear();
+            Console.WriteLine("Выберите цвет");
+            Console.WriteLine("1 - Белый");
+            Console.WriteLine("2 - Синий");
+            Console.WriteLine("3 - Красный");
+            Console.WriteLine("4 - Черный");
+        }
+
+        static void ShowConsoleMenu()
+        {   
+            Console.Clear();
+            Console.WriteLine("Информация по типам");
             Console.WriteLine("1 - Общая информация по типам");
             Console.WriteLine("2 - Выбрать тип из списка");
             Console.WriteLine("3 - Параметры консоли");
             Console.WriteLine("0 - Выход из программы");
+        }
+
+        static void ShowChangeConsoleViewMenu() 
+        {
+            Console.Clear();
+            Console.WriteLine("Выберите пункт");
+            Console.WriteLine("1 - Вернуться в меню");
+            Console.WriteLine("2 - Изменить цвет шрифта");
+            Console.WriteLine("3 - Изменить цвет фона");
         }
     }
 }
