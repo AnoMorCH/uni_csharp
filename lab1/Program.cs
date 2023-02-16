b82234aaba90d0851d4f9168b8760b32ba857575
Anton Morozov <anomorch@gmail.com>
2023-02-16 20:15:04 +0300

    Write StartApp() method

--- lab1/Program.cs
+++ lab1/Program.cs
@@ -6,7 +6,34 @@ namespace FirstLab // Note: actual namespace depends on the project name.
     {
         static void Main(string[] args)
         {
+            StartApp();
+        }
+
+        static void StartApp() 
+        {
             ShowConsoleMenu();
+            WaitInput();
+        }
+
+        static void WaitInput() {
+            while(true)
+            {
+                char inputedKey = char.ToLower(Console.ReadKey().KeyChar); 
+                switch(inputedKey)
+                {
+                    case '1': 
+                        // ShowAllTypeInfo();
+                        break;
+                    case '2':
+                        // SelectType();
+                        break;
+                    case '3':
+                        // ChangeConsoleView();
+                        break;
+                    default:
+                        break;
+                }
+            }
         }
 
         static void ShowConsoleMenu()
