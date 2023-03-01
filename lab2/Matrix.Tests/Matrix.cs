06cf0bb86f7c3733aec55711bce746ccacb6ce81
Anton Morozov <anomorch@gmail.com>
2023-03-01 17:44:15 +0300

    lab2: test if a value inserted right to the matrix

--- lab2/Matrix.Tests/Matrix.cs
+++ lab2/Matrix.Tests/Matrix.cs
@@ -4,11 +4,19 @@ namespace Matrix.UnitTests
     public class MatrixUnitTests
     {
         [TestMethod]
-        public void CreateCertainMatrix()
+        public void doesCreatedMatrixHaveCorrectDimensions()
         {
             Matrix matrix = new Matrix(2, 4);
             Assert.AreEqual(2, matrix.columnsAmount);
             Assert.AreEqual(4, matrix.rowsAmount);
+        }
+
+        [TestMethod]
+        public void isValueInsertedRightToMatrix()
+        {
+            Matrix matrix = new Matrix(2, 4);
+            matrix[1, 2] = 5;
+            Assert.AreEqual(matrix[1, 2], 5);
         }
     }
 }
