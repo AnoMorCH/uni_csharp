204d094eabcb33a66c4f2ea0bbfb255dc14dd518
Anton Morozov <anomorch@gmail.com>
2023-03-01 16:02:18 +0300

    lab2: create a matrix method

--- lab2/Program.cs
+++ lab2/Program.cs
@@ -4,9 +4,46 @@ namespace SecondLab
 {
     internal class Program
     {
-        static void Main(string[] args)
+        public class Matrix 
         {
-            Console.WriteLine("Hello World!");
+            int rowsAmount;
+            int columnsAmount;
+            double[] data;
+
+            public Matrix(int columnsAmount, int rowsAmount)
+            {
+                int dataLength = rowsAmount * columnsAmount;
+
+                this.rowsAmount = rowsAmount;
+                this.columnsAmount = columnsAmount;
+                data = new double[dataLength];
+            } 
+
+            public double this[int columnNumber, int rowNumber]
+            {
+                
+                get 
+                {
+                    int index = rowNumber * columnsAmount + columnNumber;
+                    return data[index];
+                }
+                
+                set
+                {
+                    int index = rowNumber * columnsAmount + columnNumber;
+                    data[index] = value;
+                }
+            }
+        }
+
+        static int GetTaskOption(char nameFirstChar, char surnameFirstChar)
+        {
+            return (nameFirstChar + surnameFirstChar) % 7;
+        }
+
+        public static void Main(string[] args)
+        {
+            Console.WriteLine($"Вариант - {GetTaskOption('A', 'M')}");
         }
     }
 }
